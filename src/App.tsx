f2d06a975f561791801ee14c14f05a419e6223b4
Ivo Soares <ivonaldojrsoares@gmail.com>
2022-06-06 23:02:42 -0300

    adiciona formulario e exemplo de componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Button from './components/shared/Button';
 import Home from './pages/Home';
 import Router from './router';
 
 function App() {
   return (
     <div className="container">
-      <h1>Blueworking</h1>
+      <h1 className="logo">Blueworking</h1>
+      <Button texto="meu texto"/>
       <div className="content">
         <Router/>
       </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,42 @@
+import { SyntheticEvent, useState } from 'react'
+import Button from '../../components/shared/Button/index';
 function Home() {
+  const [email, setEmail] = useState('');
+
+
+  // devemos receber o valor digitado pelo usuario no input de email e enviar uma requisção para api do tipo POST.
+  // para cadastrar o usuario ou consultar o usuário caso já exista.
+  const handleSubmit = (evento: SyntheticEvent) => {
+    evento.preventDefault();
+    console.log(evento);
+  }
+
+  const onChangeInput = (event: any) => {
+    setEmail(event.target.value);
+  }
+
   return(
     <>
-      <p>Cadastre espaços para devs e encontre grande profissionais.</p>
-      <form>
-        <label htmlFor="meu-input">E-mail</label>
-        <input type="text" id="meu-input" />
+      <p>Cadastre espaços para <strong>devs</strong> e encontre <strong>grandes</strong> profissionais.</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-mail:*</label>
+        <input 
+          type="email" 
+          id="email"
+          placeholder="Informe o seu melhor e-mail"
+          onChange={onChangeInput}
+          required
+          autoComplete="off"
+        />
+        <button type="submit" className="btn">Entrar</button>
+        <p>{email}</p>
       </form>
+
+      <Button texto="Enviar" estilo="sucesso"/>
+      <Button texto="cancelar" estilo="perigo"/>
+      <Button texto="teste" estilo="atencao"/>
     </>
   )
 }
-
 
 export default Home;
